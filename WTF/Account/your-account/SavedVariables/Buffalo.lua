d7682d63763c6a7a5b8def973d8fcedb7c1894dd
Dan van der Heyden <danvdh@gmail.com>
2017-02-03 00:25:58 -0500

    Revert "Remove Buffalo"
    This reverts commit 27b77c5e45c6d9f1cd406b7ad9da54c85df2151a.

---
+++ WTF/Account/your-account/SavedVariables/Buffalo.lua
@@ -0,0 +1,33 @@
+
+BuffaloDB = {
+	["profiles"] = {
+		["Default"] = {
+			["xPos"] = {
+				["debuff"] = 1707.041716147684,
+				["buff"] = 1707.041996261817,
+				["weapon"] = 1788.170612211458,
+			},
+			["scale"] = {
+				["debuff"] = 1.200000047683716,
+				["buff"] = 1.2,
+			},
+			["flashes"] = {
+				["debuff"] = false,
+				["buff"] = false,
+				["weapon"] = false,
+			},
+			["locked"] = true,
+			["yPos"] = {
+				["debuff"] = 807.871358131595,
+				["buff"] = 1028.826296480157,
+				["weapon"] = 806.9483820639146,
+			},
+			["growRight"] = {
+				["weapon"] = true,
+			},
+			["hide"] = {
+				["debuff"] = false,
+			},
+		},
+	},
+}
