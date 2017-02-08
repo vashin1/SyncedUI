5bdcdf830c4ed5fe5064bc03fc382707b2eb2f2d
Dan van der Heyden <danvdh@gmail.com>
2017-02-07 22:59:34 -0500

    Addons: Update pfUI

--- Interface/AddOns/pfUI/env/fonts.lua
+++ Interface/AddOns/pfUI/env/fonts.lua
@@ -2,7 +2,7 @@
 if GetLocale() == "zhCN" then
   STANDARD_TEXT_FONT = "Fonts\\FZXHLJW.TTF"
 else
-  STANDARD_TEXT_FONT = "Interface\\AddOns\\pfUI\\fonts\\PT_Sans_Narrow.ttf"
+  STANDARD_TEXT_FONT = "Interface\\AddOns\\pfUI\\fonts\\Myriad-Pro.ttf"
 end
 
 function pfUI.environment:UpdateFonts()
@@ -11,9 +11,9 @@ function pfUI.environment:UpdateFonts()
     pfUI.font_combat =  "Interface\\AddOns\\pfUI\\fonts\\" .. pfUI_config.global.font_combat .. ".ttf"
     pfUI.font_square = "Interface\\AddOns\\pfUI\\fonts\\" .. pfUI_config.global.font_square .. ".ttf"
   else
-    pfUI.font_default = "Interface\\AddOns\\pfUI\\fonts\\PT_Sans_Narrow.ttf"
+    pfUI.font_default = "Interface\\AddOns\\pfUI\\fonts\\Myriad-Pro.ttf"
     pfUI.font_square = "Interface\\AddOns\\pfUI\\fonts\\Expressway.ttf"
-    pfUI.font_combat = "Interface\\AddOns\\pfUI\\fonts\\diediedie.ttf"
+    pfUI.font_combat = "Interface\\AddOns\\pfUI\\fonts\\Expressway.ttf"
   end
 
   -- force locale based fonts
@@ -28,7 +28,7 @@ function pfUI.environment:UpdateFonts()
   STANDARD_TEXT_FONT = pfUI.font_default;
   UNIT_NAME_FONT = pfUI.font_default;
   DAMAGE_TEXT_FONT = pfUI.font_combat;
-  NAMEPLATE_FONT = pfUI.font_square;
+  NAMEPLATE_FONT = pfUI.font_default;
 
   SystemFont:SetFont(pfUI.font_default, 15)
   GameFontNormal:SetFont(pfUI.font_default, 12)
@@ -47,16 +47,16 @@ function pfUI.environment:UpdateFonts()
   MailTextFontNormal:SetFont(pfUI.font_default, 15)
   SubSpellFont:SetFont(pfUI.font_default, 12)
   DialogButtonNormalText:SetFont(pfUI.font_default, 16)
-  ZoneTextFont:SetFont(pfUI.font_square, 34, "OUTLINE")
-  SubZoneTextFont:SetFont(pfUI.font_square, 24, "OUTLINE")
+  ZoneTextFont:SetFont(pfUI.font_default, 34, "OUTLINE")
+  SubZoneTextFont:SetFont(pfUI.font_default, 24, "OUTLINE")
   TextStatusBarTextSmall:SetFont(pfUI.font_default, 12, "NORMAL")
-  GameTooltipText:SetFont(pfUI.font_square, 12)
-  GameTooltipTextSmall:SetFont(pfUI.font_square, 12)
-  GameTooltipHeaderText:SetFont(pfUI.font_square, 13)
+  GameTooltipText:SetFont(pfUI.font_default, 12)
+  GameTooltipTextSmall:SetFont(pfUI.font_default, 12)
+  GameTooltipHeaderText:SetFont(pfUI.font_default, 13)
   WorldMapTextFont:SetFont(pfUI.font_default, 102, "THICK")
   InvoiceTextFontNormal:SetFont(pfUI.font_default, 12)
   InvoiceTextFontSmall:SetFont(pfUI.font_default, 12)
-  ChatFontNormal:SetFont(pfUI.font_square, 12, "OUTLINE")
+  ChatFontNormal:SetFont(pfUI.font_default, 12, "OUTLINE")
   CombatTextFont:SetFont(pfUI.font_combat, 22)
 end
 
