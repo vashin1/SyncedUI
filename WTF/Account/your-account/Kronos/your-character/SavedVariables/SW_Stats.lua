4730b6dcab5de285a68edd201579ddf7b4c3a1ca
Marelle <danvdh@gmail.com>
2017-05-29 09:19:24 -0400

    Bring back sw_stats
    until DPSMate is more stable

---
+++ WTF/Account/your-account/Kronos/your-character/SavedVariables/SW_Stats.lua
@@ -0,0 +1,223 @@
+
+SW_Settings = {
+	["SW_IconPos"] = 20,
+	["QuickOptCount"] = 0,
+	["ReportAmount"] = 5,
+	["HideMiniMap"] = true,
+	["TL_SafeMode"] = true,
+	["BarFrames"] = {
+		["SW_BarFrame1"] = {
+			["Docked"] = {
+				[1] = 1,
+			},
+			["Selected"] = 1,
+		},
+	},
+	["lastWOWBuild"] = "1.12.15875Sep 19 2006",
+	["SW_TL_AutoDelete"] = true,
+	["LAST_V_RUN"] = "2.0 Beta.7",
+	["SHOWMAIN"] = true,
+	["BFLocked"] = true,
+	["Colors"] = {
+		["HUNTER"] = {
+			[1] = 0.67,
+			[2] = 0.83,
+			[3] = 0.45,
+			[4] = 1,
+		},
+		["WARRIOR"] = {
+			[1] = 0.78,
+			[2] = 0.61,
+			[3] = 0.43,
+			[4] = 1,
+		},
+		["ROGUE"] = {
+			[1] = 1,
+			[2] = 0.96,
+			[3] = 0.41,
+			[4] = 1,
+		},
+		["MAGE"] = {
+			[1] = 0.41,
+			[2] = 0.8,
+			[3] = 0.94,
+			[4] = 1,
+		},
+		["DRUID"] = {
+			[1] = 1,
+			[2] = 0.49,
+			[3] = 0.04,
+			[4] = 1,
+		},
+		["Damage"] = {
+			[1] = 1,
+			[2] = 0,
+			[3] = 0,
+			[4] = 1,
+		},
+		["TitleBarsFont"] = {
+			[1] = 1,
+			[2] = 1,
+			[3] = 1,
+			[4] = 1,
+		},
+		["MainWinBack"] = {
+			[1] = 0,
+			[2] = 0,
+			[3] = 0,
+			[4] = 1,
+		},
+		["PRIEST"] = {
+			[1] = 1,
+			[2] = 1,
+			[3] = 1,
+			[4] = 1,
+		},
+		["Heal"] = {
+			[1] = 0,
+			[2] = 1,
+			[3] = 0,
+			[4] = 1,
+		},
+		["WARLOCK"] = {
+			[1] = 0.58,
+			[2] = 0.51,
+			[3] = 0.79,
+			[4] = 1,
+		},
+		["SHAMAN"] = {
+			[1] = 0.14,
+			[2] = 0.35,
+			[3] = 1,
+			[4] = 1,
+		},
+		["Backdrops"] = {
+			[1] = 0.6980392156862745,
+			[2] = 0.6980392156862745,
+			[3] = 1,
+			[4] = 0,
+		},
+		["TitleBars"] = {
+			[1] = 0,
+			[2] = 0,
+			[3] = 0,
+			[4] = 1,
+		},
+		["PALADIN"] = {
+			[1] = 0.96,
+			[2] = 0.55,
+			[3] = 0.73,
+			[4] = 1,
+		},
+	},
+	["OPT_ShowMainWinDPS"] = 1,
+	["IsRunning"] = true,
+	["SW_IconPosR"] = 80,
+	["OPT_ShowTLB"] = 1,
+	["InfoSettings"] = {
+		[1] = {
+			["CF"] = 1,
+			["BC"] = {
+				[1] = 0,
+				[2] = 0,
+				[3] = 0,
+				[4] = 1,
+			},
+			["OTF"] = "",
+			["UCC"] = 1,
+			["OC"] = {
+				[1] = 1,
+				[2] = 0,
+				[3] = 0,
+				[4] = 1,
+			},
+			["BFS"] = 12,
+			["BT"] = 13,
+			["PF"] = "SW_PF_Inactive",
+			["ShowRank"] = 1,
+			["ShowNumber"] = 1,
+			["BFC"] = {
+				[1] = 1,
+				[2] = 1,
+				[3] = 1,
+				[4] = 1,
+			},
+			["SF"] = "SW_Filter_Group",
+			["COLC"] = 1,
+			["IN"] = 1,
+			["BH"] = 11,
+		},
+	},
+}
+SW_RPS = {
+	["lastFightSecs"] = 14.79700000000048,
+	["allowLastFightUpdate"] = true,
+	["isRunning"] = false,
+	["startTimer"] = {
+		["uTS"] = -9610362.203999996,
+		["msO"] = -0.2039999999997235,
+		["epochTS"] = 1486453342,
+	},
+	["uglyTruthStarted"] = true,
+	["lastFightDmg"] = 0,
+	["baseTimer"] = {
+		["uTS"] = -10306647.32800007,
+		["msO"] = -0.3280000000004293,
+		["epochTS"] = 1485757057,
+	},
+	["lastEvent"] = 302.5930000000008,
+	["totalSecs"] = 176.4059999999972,
+	["maxDPS"] = 0,
+	["startDmg"] = 0,
+	["currentSecs"] = 0,
+}
+SW_StrTable = {
+	[1] = "Marelle",
+	[2] = "Vashin",
+	[3] = "WORLD",
+	[4] = "Thorns",
+}
+SW_DataCollection = {
+	["settings"] = {
+		["smallestSelect"] = 0,
+		["isInGroup"] = true,
+		["lastZone"] = "",
+		["startMarker"] = 1,
+		["biggestSelect"] = 0,
+		["isInRaid"] = false,
+		["endMarker"] = 1,
+		["activeOnly"] = true,
+	},
+	["meta"] = {
+		[1] = {
+			["origName"] = "Marelle",
+			["stringID"] = 1,
+			["everGroup"] = true,
+			["classE"] = "MAGE",
+			["level"] = 60,
+			["type"] = "PC",
+			["rank"] = 2,
+		},
+		[2] = {
+			["origName"] = "Vashin",
+			["stringID"] = 2,
+			["everGroup"] = true,
+			["classE"] = "WARRIOR",
+			["level"] = 0,
+			["type"] = "PC",
+			["rank"] = 0,
+		},
+	},
+	["data"] = {
+		[1] = {
+			["initTS"] = {
+				["uTS"] = 7.936999999999898,
+				["msO"] = -0.06300000000010186,
+				["epochTS"] = 1496063712,
+			},
+			["Name"] = "SyncInit",
+		},
+	},
+}
+SW_DBG_Log = {
+}
